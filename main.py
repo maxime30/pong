c883c8274c5df21bbd4e52bd9930f145585310d4
maxime30 <108862001+maxime30@users.noreply.github.com>
2022-07-07 11:28:44 +0200

    ball bounced

--- main.py
+++ main.py
@@ -17,58 +17,7 @@ sleep(500)
 
 
 while True:
-    while ball_y < 3:
-        if button_b.was_pressed() and paddle_x < 3:
-            paddle_x += 1
-            display.clear()
-
-        if button_a.was_pressed() and paddle_x > 0:
-            paddle_x -= 1
-            display.clear()
-        display.clear()
-        for i in range(paddle_length):
-            display.set_pixel(paddle_x + i, paddle_y, 9)
-        display.set_pixel(paddle_x, paddle_y, 9)
-        ball_y += 1
-        display.set_pixel(ball_x, ball_y, 9)
-        sleep(500)
-    while ball_y > 0:
-        if button_b.was_pressed() and paddle_x < 3:
-            paddle_x += 1
-            display.clear()
-
-        if button_a.was_pressed() and paddle_x > 0:
-            paddle_x -= 1
-            display.clear()
-        display.clear()
-        for i in range(paddle_length):
-            display.set_pixel(paddle_x + i, paddle_y, 9)
-        display.set_pixel(paddle_x, paddle_y, 9)
-        ball_y -= 1
-        display.set_pixel(ball_x, ball_y, 9)
-        sleep(500)
-
-
-while True:
-    while ball_x < 4:
-        if button_b.was_pressed() and paddle_x < 3:
-            paddle_x += 1
-            display.clear()
-
-        if button_a.was_pressed() and paddle_x > 0:
-            paddle_x -= 1
-            display.clear()
-        for i in range(paddle_length):
-            display.set_pixel(paddle_x + i, paddle_y, 9)
-
-        display.set_pixel(ball_x, ball_y, 9)
-        sleep(500)
-        display.clear()
-        ball_x += 1
-        ball_y += 1
-
     while ball_y < 4:
-
         if button_b.was_pressed() and paddle_x < 3:
             paddle_x += 1
             display.clear()
@@ -76,33 +25,23 @@ while True:
         if button_a.was_pressed() and paddle_x > 0:
             paddle_x -= 1
             display.clear()
+
+        display.clear()
+
         for i in range(paddle_length):
             display.set_pixel(paddle_x + i, paddle_y, 9)
 
-        display.set_pixel(ball_x, ball_y, 9)
-        sleep(500)
-        display.clear()
-        ball_x -= 1
+        if ball_x in range(paddle_x, paddle_x + paddle_length) and ball_y == 3:
+            break
+
         ball_y += 1
-    while ball_x > 0:
-
-        if button_b.was_pressed() and paddle_x < 3:
-            paddle_x += 1
-            display.clear()
-
-        if button_a.was_pressed() and paddle_x > 0:
-            paddle_x -= 1
-            display.clear()
-        for i in range(paddle_length):
-            display.set_pixel(paddle_x + i, paddle_y, 9)
-
         display.set_pixel(ball_x, ball_y, 9)
         sleep(500)
-        display.clear()
-        ball_x -= 1
-        ball_y -= 1
+
+    if ball_y == 4:
+        break
+
     while ball_y > 0:
-
         if button_b.was_pressed() and paddle_x < 3:
             paddle_x += 1
             display.clear()
@@ -110,11 +49,15 @@ while True:
         if button_a.was_pressed() and paddle_x > 0:
             paddle_x -= 1
             display.clear()
+        display.clear()
         for i in range(paddle_length):
-            display.set_pixel(paddle_x + i, paddle_y, 9)
-
+                 display.set_pixel(paddle_x + i, paddle_y, 9)
+        ball_y -= 1
         display.set_pixel(ball_x, ball_y, 9)
         sleep(500)
-        display.clear()
-        ball_x += 1
-        ball_y -= 1
+
+
+# game over
+display.clear()
+display.show(Image.SAD)
+
